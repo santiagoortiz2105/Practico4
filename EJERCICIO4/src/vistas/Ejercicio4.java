b285286c9a9ad705a0ce7a7887f8dadee8e8187e
Santiago Ortiz <santiagoortiz2105@gmail.com>
2025-09-03 15:22:14 -0300

    Practico4
    Agregada la funcion salir del Jframe form

--- EJERCICIO4/src/vistas/Ejercicio4.java
+++ EJERCICIO4/src/vistas/Ejercicio4.java
@@ -147,7 +147,7 @@ public class Ejercicio4 extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     /**
