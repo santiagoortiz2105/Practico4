7fbd8b22261c8f637719438e5e0d5071cafca166
Santiago Ortiz <santiagoortiz2105@gmail.com>
2025-09-03 15:20:16 -0300

    Practico4
    agregada funcion salir en vistas alumno y materia.

--- EJERCICIO4/src/vistas/VistaAlumnos.java
+++ EJERCICIO4/src/vistas/VistaAlumnos.java
@@ -152,7 +152,7 @@ public class VistaAlumnos extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
 
--- EJERCICIO4/src/vistas/VistaMaterias.java
+++ EJERCICIO4/src/vistas/VistaMaterias.java
@@ -76,6 +76,11 @@ public class VistaMaterias extends javax.swing.JInternalFrame {
         jButton3.setBackground(new java.awt.Color(204, 204, 204));
         jButton3.setForeground(new java.awt.Color(0, 0, 0));
         jButton3.setText("Salir");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -138,6 +143,10 @@ public class VistaMaterias extends javax.swing.JInternalFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+      this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
